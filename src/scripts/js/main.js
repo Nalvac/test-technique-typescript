372a10fe99f4fbb3bf2ec2e92aef1829ca30cb77
Nalvac <vacna1997@gmail.com>
2022-01-13 16:37:38 +0100

    Test technique front-end Ubilab

--- src/scripts/js/main.js
+++ src/scripts/js/main.js
@@ -1,3 +1,13 @@
 "use strict";
 console.log('Test technique front-end Ubilab');
 //# sourceMappingURL=main.js.map
+var menu = document.getElementById("menu");
+var liens = menu.getElementsByClassName("lien");
+for (var i = 0; i < liens.length; i++) {
+    liens[i].addEventListener("click", function(e) {
+    e.preventDefault();
+     var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
