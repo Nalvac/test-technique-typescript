acc248c8fe0ad67b71e7dce4b4565c89cf75b5be
Nalvac <vacna1997@gmail.com>
2022-01-14 01:13:45 +0100

    Test-end

--- src/scripts/ts/result/result.service.spec.ts
+++ src/scripts/ts/result/result.service.spec.ts
@@ -29,7 +29,8 @@ describe('ResultService', () => {
     it('devrait avoir une liste de 1 résultat vue aprés la vision de ce résultat', () => {
       resultService.seenResult(46);
       expect(resultService.getAllResultSeen().length).toEqual(1);
-      expect(resultService.getAllResult()[1].isSeen).toEqual(true);
+      /* A cet instant du test dans getAllResult nous avons 1 élément donc l'index sera 0*/
+      expect(resultService.getAllResult()[0].isSeen).toEqual(true);
     });
   });
 
@@ -37,30 +38,69 @@ describe('ResultService', () => {
   describe("aprés l'ajout de 3 resultats,", () => {
     beforeEach(() => {
       // init le service avec 3 resultats
+      const result1: ResultModel = {id: 46,idOwner:76,idRecipients:[42],isSeen:false,eventResults:[],contentOfResult:"Test1"};
+      const result2: ResultModel = {id: 4,idOwner:76,idRecipients:[42],isSeen:false,eventResults:[],contentOfResult:"Test2"};      
+      const result3: ResultModel = {id: 6,idOwner:76,idRecipients:[42],isSeen:false,eventResults:[],contentOfResult:"Test3"};
+      resultService = new ResultService();
+      resultService.addResult(result1);
+      resultService.addResult(result2);
+      resultService.addResult(result3);
     });
 
     it("devrait avoir une liste de 3 resultats non vue aprés l\'ajout de 3 resultat.", () => {
-      expect(false).toEqual(true);
+      //expect(false).toEqual(true);
+      // getAllResult doit avoir trois élements
+      expect(resultService.getAllResult().length).toEqual(3);
+      // 3 resultats non vue-vérification     
+      for(let i in resultService.getAllResult() ){
+        expect(resultService.getAllResult()[i].isSeen).toEqual(false);
+      }
     });
 
     it("ne devrait pas authorisé l'ajout d'un résultats avec un id existent", () => {
-      expect(false).toEqual(true);
+      //expect(false).toEqual(true);
+      expect(resultService.getAllResult().length).toEqual(3);
     });
 
     it("devrait avoir 1 resultats vue dans la liste aprés la vision d\'un resultat", () => {
-      expect(false).toEqual(true);
+      //expect(false).toEqual(true);
+      let seen : boolean=false;
+      resultService.seenResult(6);      
+      resultService.seenResult(4);           
+      resultService.seenResult(46);
+      for(let i in resultService.getAllResult()) {
+        if(resultService.getAllResult()[i].isSeen){
+          expect(true).toEqual(true);
+          seen = true;
+          return
+        }
+      }
+      if(seen == false){
+        expect(false).toEqual(true);
+      } 
     });
 
     it("devrait avoir les 3 resultats vue dans la liste aprés qu\'il soit tous vue", () => {
-      expect(false).toEqual(true);
+     
+      resultService.seenResult(6);
+      resultService.seenResult(46);
+      resultService.seenResult(4);      
+      expect(resultService.getAllResultSeen().length).toEqual(3);
     });
 
     it("devrait avoir plus que 2 resultats vue dans la liste aprés qu\'il soit tous vue puis 1 ou la vue est enlevé", () => {
-      expect(false).toEqual(true);
+      //expect(false).toEqual(true);
+      resultService.seenResult(6);
+      resultService.seenResult(46);
+      resultService.seenResult(4);
+      resultService.unseenResult(46);
+      expect(resultService.getAllResultSeen().length).toEqual(2);
     });
 
     it("ne devrait pas planté aprés la vision d\'un resultat non ajouté", () => {
-      expect(false).toEqual(true);
+  
+      expect(resultService.seenResult(25)).toEqual(false);
+
     });
   });
 
@@ -70,23 +110,73 @@ describe('ResultService', () => {
 
     beforeEach(() => {
       // init le service avec 3 resultats (doit etre identique que le step 2)
+      const result1: ResultModel = {id: 46,idOwner:76,idRecipients:[42],isSeen:false,eventResults:[],contentOfResult:"Test1"};
+      const result2: ResultModel = {id: 4,idOwner:7,idRecipients:[42],isSeen:false,eventResults:[],contentOfResult:"Test2"};      
+      const result3: ResultModel = {id: 6,idOwner:6,idRecipients:[42],isSeen:false,eventResults:[],contentOfResult:"Test3"};
+      resultService = new ResultService();
+      resultService.addResult(result1);
+      resultService.addResult(result2);
+      resultService.addResult(result3);
     });
 
     //ps : je ne veux pas que les event de création soi initialisé dans le beforeEach ci dessus mais directement dans le resultService
     it("devrait avoir la list des résultat dans l\'order de création ( en se basant sur les events de création)", () => {
-      expect(false).toEqual(true);
+      
+      resultService.seenResult(46);
+      let results = resultService.getAllResult();
+      for (let i = 0; i < results[i].eventResults.length - 1; i++) {
+      
+      if(results[i].eventResults.length -1){
+          break;
+      }
+       expect(results[i].eventResults[0].createdAt.getTime() < results[i+1].eventResults[0].createdAt.getTime()).toEqual(true);
+
+        
+       
+       }
     });
 
     it("devrait avoir 1 event a la date de maintenant quand 1 résultat est vue", () => {
-      expect(false).toEqual(true);
+      //expect(false).toEqual(true);      
+      resultService.seenResult(6);
+      let results = resultService.getAllResult();
+      for (let i = 0; i < results[i].eventResults.length - 1; i++) {
+        if (results[i].eventResults[0].createdAt.getTime() == new Date().getTime()) {
+          expect(true).toEqual(true);
+
+        }else{
+          expect(false).toEqual(true);
+        }
+       }
     });
 
     it("devrait avoir 2 events avec 2 dates différent aprés la vision d\'un resultat puis la suppression de la vision", () => {
-      expect(false).toEqual(true);
+      resultService.seenResult(46);      
+      resultService.unseenResult(46);
+      let seen = false;
+      // Si getAllResult()[i].eventResults.length return 2 =>> avoir 2 events avec 2 dates différent
+      for(let i in resultService.getAllResult()) {
+          if (resultService.getAllResult()[i].eventResults.length == 2){
+            seen = true;
+          }
+     
+      }
+      expect(seen).toEqual(true);
     });
 
     it("devrait avoir une fonction qui retourne une liste ordonnée des resultats par rapport au dernier modifier", () => {
-      expect(false).toEqual(true);
+      // Je ne suis pas convaincu de celui ci 
+      let results = resultService.dernierModif();
+      for (let i = 0; i < results[i].eventResults.length - 1; i++) {
+      
+      if(results[i].eventResults.length -1){
+          break;
+      }
+       expect(results[i].eventResults[0].createdAt.getTime() > results[i+1].eventResults[0].createdAt.getTime()).toEqual(true);
+
+        
+       
+       }
     });
   });
 
--- src/scripts/ts/result/result.service.ts
+++ src/scripts/ts/result/result.service.ts
@@ -1,36 +1,108 @@
 import { ResultModel } from './model/result.model';
 import { ResultEventModel } from './model/result-event.model';
 
+enum Events {
+  Created = 'created',
+  Seen = 'seen',
+  Received = 'received',
+}
 export class ResultService {
+  public result : ResultModel = {
+    id : 0,
+    idOwner :0,
+    idRecipients:[],
+    isSeen:false,
+    eventResults: [],
+    contentOfResult:"",
+    
+  };
+  
+  public newResults  : ResultModel [] ;
+  public resultIsSeen  : ResultModel [] ;  
+  public resultUnSeen  : ResultModel [] ;
+   
+  constructor() { 
+    this.newResults= [];
+    this.resultIsSeen = [];
+    this.resultUnSeen = [];
 
-  constructor() { }
+  }
+  
 
-  public addResult(newResult:ResultModel) {
-
+  public addResult(newResult:ResultModel) {  
+    if (this.newResults.some(el=>el.id == newResult.id)){
+      console.log("Existe déjà");
+    }
+    else{
+       let event = {
+        id: Events.Created,
+        idOwner: newResult.idOwner,
+        createdAt:new Date()
+      }
+      newResult.eventResults.push(event);
+      this.newResults.push(newResult);
+    }
   }
 
   public seenResult(idResult:number) {
+    let  result  = this.newResults.find((el)=>el.id==idResult);
+    if (result != undefined){
+      result.isSeen = true;
+      result.eventResults[0].createdAt = new Date();
+      this.resultIsSeen.push(result);
+      const indexElemt= this.newResults.findIndex((el)=>el.id == idResult);
+      if(indexElemt > -1){
+        this.newResults.splice(indexElemt,1); 
+      }
+      return true;
+    } else {
+      return false;
+    }
 
   }
 
   public unseenResult(idResult:number) {
-
+    let  result  = this.resultIsSeen.find((el)=>el.id==idResult);
+    if (result != undefined){
+      result.isSeen = false;
+      let event = {
+        id: Events.Seen,
+        idOwner: result.idOwner,
+        createdAt:new Date()
+      }
+      result.eventResults.push(event);
+      this.resultUnSeen.push(result);
+     const indexElemt= this.resultIsSeen.findIndex((el)=>el.id == idResult);
+     if(indexElemt > -1){
+      this.resultIsSeen.splice(indexElemt,1); 
+    }
+    }
   }
 
   public getAllResult() : Array<ResultModel> {
-    return [];
+    
+      
+    console.log(JSON.stringify(this.newResults.concat(this.resultIsSeen).concat(this.resultUnSeen)) );
+    return this.newResults.concat(this.resultIsSeen).concat(this.resultUnSeen) ;
   }
 
   public getAllResultSeen() : Array<ResultModel> {
-    return [];
+    
+    return this.resultIsSeen;
   }
 
   public getAllResultUnSeen() : Array<ResultModel> {
-    return [];
+    console.log(this.resultUnSeen);
+    return this.resultUnSeen;
   }
 
   public numberOfEventSeen() : number
   {
     return 0;
+  }
+  public dernierModif():Array<ResultModel>{
+
+    
+    return this.getAllResult().reverse();
   }
 }
